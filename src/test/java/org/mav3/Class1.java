6d9b10e386657c05296e34654a6f993489fc6e3c
Shankar182 <shankar21vm@gmail.com>
2021-11-12 12:00:14 +0530

    first in github

---
+++ src/test/java/org/mav3/Class1.java
@@ -0,0 +1,21 @@
+package org.mav3;
+
+public class Class1 {
+	
+	public void method1() {
+
+	}
+	
+	public void method2() {
+
+	}
+	
+	public void method3() {
+
+	}
+	
+	public void method4() {
+
+	}
+
+}
